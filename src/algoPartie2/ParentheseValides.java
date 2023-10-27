09a518a2f854e37489c7ace99c0e8baa182a29a1
Camille LEOST <camille.leost@alliance4u.fr>
2023-10-27 15:53:52 +0200

    suite algo

---
+++ src/algoPartie2/ParentheseValides.java
@@ -0,0 +1,41 @@
+package algoPartie2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ParentheseValides {
+    public static void main(String[] args) {
+     /*   Example 1:
+
+        Input: s = "()"
+        Output: true
+        Example 2:
+
+        Input: s = "()[]{}"
+        Output: true
+        Example 3:
+
+        Input: s = "(]"
+        Output: false*/
+        String s  = "([)]";
+
+        System.out.println(isValid(s));
+
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(map.get(c)); //stack )
+            }
+            else if (stack.empty() || stack.pop() != c) return false;
+        }
+        return stack.empty();
+    }
+}
