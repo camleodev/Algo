d6564452e5d75eccce695b25f6d6a1d2fdcee173
Camille LEOST <camille.leost@alliance4u.fr>
2023-11-21 16:40:14 +0100

    suite algo

--- src/algoExpert/facile/ClassPhotos.java
+++ src/algoExpert/facile/ClassPhotos.java
@@ -1,9 +1,7 @@
 package algoExpert.facile;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class ClassPhotos {
     public static void main(String[] args) {
@@ -25,33 +23,38 @@ public class ClassPhotos {
 
         //OUTPUT true
 
-
         ArrayList<Integer> redShirtHeights = new ArrayList<>();
-        redShirtHeights.add(5); redShirtHeights.add(8); redShirtHeights.add(1); redShirtHeights.add(3);
+        redShirtHeights.add(5);
+        redShirtHeights.add(8);
+        redShirtHeights.add(1);
+        redShirtHeights.add(3);
         redShirtHeights.add(4);
 
         ArrayList<Integer> blueShirtHeights = new ArrayList<>();
-        blueShirtHeights.add(6);  blueShirtHeights.add(9);  blueShirtHeights.add(2);  blueShirtHeights.add(4);
+        blueShirtHeights.add(6);
+        blueShirtHeights.add(9);
+        blueShirtHeights.add(2);
+        blueShirtHeights.add(4);
         blueShirtHeights.add(5);
-        classPhotos(redShirtHeights,blueShirtHeights);
+        System.out.println(classPhotos(redShirtHeights, blueShirtHeights));
 
     }
 
     public static boolean classPhotos(
             ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights
     ) {
-       Collections.sort(redShirtHeights);
+        Collections.sort(redShirtHeights);
         Collections.sort(blueShirtHeights);
         boolean red = true;
         boolean blue = true;
         for (int i = 0; i < redShirtHeights.size(); i++) {
-            if(redShirtHeights.get(i)<= blueShirtHeights.get(i)){
+            if (redShirtHeights.get(i) <= blueShirtHeights.get(i)) {
                 red = false;
             }
         }
 
         for (int i = 0; i < redShirtHeights.size(); i++) {
-            if(blueShirtHeights.get(i)<= redShirtHeights.get(i)){
+            if (blueShirtHeights.get(i) <= redShirtHeights.get(i)) {
                 blue = false;
             }
         }
